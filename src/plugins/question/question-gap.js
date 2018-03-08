6bde5d6cec75fbc4f34785ca297da13bbf2075e1
nem035 <n.stojanovic035@gmail.com>
2018-03-08 02:55:06 -0500

    Add compiler to question gap node

--- src/plugins/question/question-gap.js
+++ src/plugins/question/question-gap.js
@@ -1,13 +1,24 @@
 module.exports = function questionGap () {
   inlineTokenizer.locator = locator
 
-  const Parser = this.Parser
+  const { Parser } = this
 
   // Inject inlineTokenizer
   const inlineTokenizers = Parser.prototype.inlineTokenizers
   const inlineMethods = Parser.prototype.inlineMethods
   inlineTokenizers.questionGap = inlineTokenizer
   inlineMethods.splice(0, 0, 'questionGap')
+
+  const { Compiler } = this
+
+  if (Compiler) {
+    const { visitors } = Compiler.prototype
+    if (visitors) {
+      visitors.questionGap = function (node) {
+        return node.value
+      }
+    }
+  }
 
   function locator (value, fromIndex) {
     return value.indexOf('???', fromIndex)
