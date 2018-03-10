e129f8e50a0ec2f37bcd739408d45375fef6f4a2
nem035 <n.stojanovic035@gmail.com>
2018-03-10 05:46:20 -0500

    Cleanup headline and yaml fail tests to allow additional describe blocks for other failure tests, if necessary

--- src/plugins/insight/__tests__/fail-headline.test.js
+++ src/plugins/insight/__tests__/fail-headline.test.js
@@ -2,27 +2,31 @@ const { loadRawFixture } = require('../../test-utils')
 const { getParser, types } = require('../../../index')
 
 describe('Fail insight headline parsing', () => {
-  const text = loadRawFixture({
-    type: 'insight',
-    file: 'error/missing-headline.md'
-  })
+  describe('Fail for insights with missing headline', () => {
+    const text = loadRawFixture({
+      type: 'insight',
+      file: 'error/missing-headline.md'
+    })
 
-  let parser
-  beforeEach(() => {
-    parser = getParser(types.INSIGHT)
-  })
+    let parser
+    beforeEach(() => {
+      parser = getParser(types.INSIGHT)
+    })
 
-  test('parseSync should throw on missing headline', () => {
-    expect(() => {
-      parser.parseSync(text)
-    }).toThrow(/Must have exactly 1 headline but found \d+ instead./)
-  })
+    test('parseSync should throw on missing headline', () => {
+      expect(() => {
+        parser.parseSync(text)
+      }).toThrow(/Must have exactly 1 headline but found \d+ instead./)
+    })
 
-  test('parse should throw on missing headline', () => {
-    expect(async () => {
-      await parser
-        .parse(text)
-        .rejects.toThrow(/Must have exactly 1 headline but found \d+ instead./)
+    test('parse should throw on missing headline', () => {
+      expect(async () => {
+        await parser
+          .parse(text)
+          .rejects.toThrow(
+            /Must have exactly 1 headline but found \d+ instead./
+          )
+      })
     })
   })
 })
