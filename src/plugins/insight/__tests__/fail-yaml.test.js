4f36f6c59cf5a6a9874a36e9b6279b9da62db66e
nem035 <n.stojanovic035@gmail.com>
2018-03-10 05:40:55 -0500

    Add yaml config and headline validation

---
+++ src/plugins/insight/__tests__/fail-yaml.test.js
@@ -0,0 +1,31 @@
+const { loadRawFixture } = require('../../test-utils')
+const {
+  getParser,
+  types
+} = require('../../../index')
+
+describe('Fail insight yaml parsing', () => {
+  const text = loadRawFixture({
+    type: 'insight',
+    file: 'error/missing-yaml.md'
+  })
+
+  let parser
+  beforeEach(() => {
+    parser = getParser(types.INSIGHT)
+  })
+
+  test('parseSync should throw on missing yaml', () => {
+    expect(() => {
+      parser.parseSync(text)
+    }).toThrow(/Must have exactly 1 yaml configuration but found \d+ instead./)
+  })
+
+  test('parse should throw on missing yaml', () => {
+    expect(async () => {
+      await parser.parse(text)
+        .rejects
+        .toThrow(/Must have exactly 1 yaml configuration but found \d+ instead./)
+    })
+  })
+})
